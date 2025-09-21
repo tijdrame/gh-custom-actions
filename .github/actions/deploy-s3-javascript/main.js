a88866c8a4fda5d5389528610102eacc734dc105
Tidiane <tijdrame@gmail.com>
2025-09-21 18:00:47 -0400

    deploy on s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,19 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+//const github = require('@actions/github');
+//pas utilise ici mais pourrait l'etre pour des actions plus complexes
+//permet de faire des appels a l'API GitHub et avoir des contextes 
 const exec = require('@actions/exec');
 
 function run() {
+    //Get inputs values
+    const bucket = core.getInput("bucket", { required: true });
+    const bucketRegion = core.getInput("bucket-region", { required: true });
+    const distFolder = core.getInput("dist-folder", { required: true });
+
+    //Upload file to S3
+    const s3Uri = `s3://${bucket}`;
+    
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
     core.notice("Hello from my custom JavaScript action!");
 }
 run();
