6e86c241dada8340f4575954d7ba0dc2c123dca9
Tidiane <tijdrame@gmail.com>
2025-09-21 18:16:25 -0400

    deploy on s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -15,5 +15,8 @@ function run() {
     
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
     core.notice("Hello from my custom JavaScript action!");
+    //set url as output
+    const websiteUrl = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
 }
 run();
